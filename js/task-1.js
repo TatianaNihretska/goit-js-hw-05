0aec6039df9124f727a164bbbdc67fb3f3eac444
Nihretska Tetiana <tetiana@Nihretskas-MacBook-Air.local>
2026-03-03 20:43:14 +0100

    hm5

---
+++ js/task-1.js
@@ -0,0 +1,70 @@
+
+const getUserNames = (users) => users.map(user => user.name);
+  
+/* const getUserNames = function(users) {
+  return users.map(function(user) {
+    return user.name;
+  });
+};*/
+/* const getUserNames = (users) => {
+  const names = [];
+  users.forEach(user => names.push(user.name));
+  return names;
+};
+*/
+/* const getUserNames = (users) => {
+  const names = [];
+  for (const user of users) {
+    names.push(user.name);
+  }
+  return names;
+};*/
+/* const getUserNames = (users) => {
+  const names = [];
+  for (let i = 0; i < users.length; i++) {
+    names.push(users[i].name);
+  }
+  return names;
+};*/
+
+
+console.log(
+  getUserNames([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    balance: 2764
+  },
+])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+
